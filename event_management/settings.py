a3796a5770ff20f797c9f9ad1efa8d6177a9f87b
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-01-15 21:43:28 +0600

    add event detail and event list and modify base.html

--- event_management/settings.py
+++ event_management/settings.py
@@ -28,10 +28,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'events',
-    'users'
+    'users',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -39,6 +41,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 ROOT_URLCONF = 'event_management.urls'
--- event_management/urls.py
+++ event_management/urls.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from django.urls import path,include
 from core.views import home
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='home'), #it defines the core base home
     
     path('events/',include("events.urls")) ## it defines all tasks.url are included automatically
-]
+]+ debug_toolbar_urls()
